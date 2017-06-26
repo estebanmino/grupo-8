6d787aef9c35a9fb296f8c671f66f675aded5ea0
Esteban Mino <efmino@uc.cl>
2017-06-25 22:33:43 -0400

    weather with httparty

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -2,16 +2,26 @@ module MatchesHelper
 
   require 'open_weather'
 
+
   def get_weather(match)
+
+  end
+
+
+
+
+  def get_weather_api(match)
 
     if match.date == Date.today
       options = {  APPID: "52cc139353dcc37cf8007ac4e5217829" }
-      weather = OpenWeather::Current.geocode(match.latitude, match.longitude, options)
+      weather = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?lat=#{match.latitude}&lon=#{match.longitude}&appid=52cc139353dcc37cf8007ac4e5217829")
+
       return weather['weather'][0]['main'], (weather['main']['temp_max']-273).to_i
     else
 
       options = {  APPID: "52cc139353dcc37cf8007ac4e5217829" }
-      weather = OpenWeather::Forecast.geocode(match.latitude, match.longitude, options)
+      weather = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast?lat=#{match.latitude}&lon=#{match.longitude}&appid=52cc139353dcc37cf8007ac4e5217829")
+
       for element in weather['list']
         if element['dt_txt'][0,10] == match.date.to_s
           if element['dt_txt'][11,16].to_i > match.time.to_s[11,8].to_i
